0672584383f3678f26d67210a1ff529faca075b9
Sayra Martinez <sayra.martinez.3011@gmail.com>
2023-04-20 16:51:50 -0700

    cambios Say

---
+++ .sfdx/typings/lwc/apex/endososControllerClass.d.ts
@@ -0,0 +1,6 @@
+declare module "@salesforce/apex/endososControllerClass.endosoDocuments" {
+  export default function endosoDocuments(param: {tipoDeEndoso: any}): Promise<any>;
+}
+declare module "@salesforce/apex/endososControllerClass.fetchFiles" {
+  export default function fetchFiles(param: {recordId: any}): Promise<any>;
+}
